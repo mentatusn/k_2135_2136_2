5f7c894a79a40c433a6effef8a2077be4d8868db
Andej Nesterenko <mentatus.n@gmail.com>
2022-06-21 21:42:21 +0300

    урок 1 | познакомились со СВОЙСТВАМИ( полей у классов нет в котлине) и модификатором lateinit

--- app/src/main/java/com/gb/k_2135_2136_2/MainActivity.kt
+++ app/src/main/java/com/gb/k_2135_2136_2/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.gb.k_2135_2136_2
 
+import android.content.Context
+import android.content.SyncRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,12 +15,30 @@ class MainActivity : AppCompatActivity() {
 
         val field1 = 5
         var field2 = 5
-        field1 = 25
+
         field2 = 235
+
+        val test1:Test = Test()
+        test1.name()
     }
 }
 
-class Test constructor( val i: Int,var ){
+//class Test constructor(val vali: Int,var vari: Int){}
+class Test @JvmOverloads constructor() {
 
+    constructor(field: String):this()
+    constructor(field: String,field2: String):this(field)
+    constructor(field: String,field2: String,field3: String):this(field,field2)
 
+    private lateinit var vali: String
+    //var vari: Int
+    fun name(){
+        vali = "какое-то значение"
+        Log.d("@@@",vali)
+    }
 }
+
+
+
+
+class Button @JvmOverloads constructor(context: Context,attributeSet: AttributeSet?=null,defStyleAttr:Int=0)
