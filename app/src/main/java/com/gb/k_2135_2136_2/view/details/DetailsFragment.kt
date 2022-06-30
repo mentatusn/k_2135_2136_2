fe69996f7cee82716d0ca30a2e1889592a77db31
Andej Nesterenko <mentatus.n@gmail.com>
2022-06-30 23:28:20 +0300

    Урок 4 | познакомились с функциями области видимости

--- app/src/main/java/com/gb/k_2135_2136_2/view/details/DetailsFragment.kt
+++ app/src/main/java/com/gb/k_2135_2136_2/view/details/DetailsFragment.kt
@@ -36,25 +36,66 @@ class DetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         //val weather=  arguments?.get(BUNDLE_WEATHER_EXTRA)
-        val weather = (arguments?.getParcelable<Weather>(BUNDLE_WEATHER_EXTRA))
+        arguments?.apply { }
+        arguments?.also { }
+        arguments?.run { }
+        val weather = arguments?.let { arg ->
+            arg.getParcelable<Weather>(BUNDLE_WEATHER_EXTRA)
+        }
+        val weather2 = arguments?.run {
+            this.getParcelable<Weather>(BUNDLE_WEATHER_EXTRA)
+            getParcelable<Weather>(BUNDLE_WEATHER_EXTRA)
+        }
         if (weather != null)
             renderData(weather)
+
     }
+    // FIXME диссонанс this - как бы приемник?
 
     private fun renderData(weather: Weather) {
-        binding.cityName.text = weather.city.name
-        binding.temperatureValue.text = weather.temperature.toString()
-        binding.feelsLikeValue.text = weather.feelsLike.toString()
-        binding.cityCoordinates.text = "${weather.city.lat}/${weather.city.lon}"
+        binding?.apply {
+            this.cityName
+            cityName
+        }
+        val resAlso = binding?.also { newIt ->
+            newIt.cityName.text = ""
+            val resLet = binding?.let { bindingMy ->
+                bindingMy.cityName.toString()
+                bindingMy.cityCoordinates.toString()
+            }
+        }
+
+        val resAlso2 = binding?.also { ewsgfweg ->
+            ewsgfweg.cityName.text = ""
+            val resLet = binding?.run {
+                cityName.toString()
+                cityCoordinates.toString()
+            }
+        }
+        val resRun = binding?.run { cityName.toString() }
+
+        with(binding) {
+            cityName.text = weather.city.name
+            temperatureValue.text = weather.temperature.toString()
+            feelsLikeValue.text = weather.feelsLike.toString()
+            cityCoordinates.text = "${weather.city.lat}/${weather.city.lon}"
+        }
     }
 
     companion object {
         const val BUNDLE_WEATHER_EXTRA = "sgrrdfge"
         fun newInstance(weather: Weather): DetailsFragment {
-            val bundle = Bundle()
-            bundle.putParcelable(BUNDLE_WEATHER_EXTRA, weather)
             val fr = DetailsFragment()
-            fr.arguments = bundle
+
+            fr.arguments = Bundle().apply {
+                putParcelable(BUNDLE_WEATHER_EXTRA, weather)
+                putParcelable(BUNDLE_WEATHER_EXTRA, weather)
+            }
+            fr.arguments = Bundle().also {
+                it.putParcelable(BUNDLE_WEATHER_EXTRA, weather)
+                it.putParcelable(BUNDLE_WEATHER_EXTRA, weather)
+            }
+
             return fr
         }
     }
