b0717a00d980509b8a3eeb7200d610bfa34b32a7
Andej Nesterenko <mentatus.n@gmail.com>
2022-07-04 21:17:15 +0300

    урок 5 начали | создали webView, в него подгрузили удаленную страницу

---
+++ app/src/main/java/com/gb/k_2135_2136_2/lesson5/WebViewActivity.kt
@@ -0,0 +1,79 @@
+package com.gb.k_2135_2136_2.lesson5
+
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.gb.k_2135_2136_2.R
+import com.gb.k_2135_2136_2.databinding.ActivityMainBinding
+import com.gb.k_2135_2136_2.databinding.ActivityWebviewBinding
+import com.gb.k_2135_2136_2.lesson4.Lesson4
+import com.gb.k_2135_2136_2.view.weatherlist.WeatherListFragment
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URI
+import java.net.URL
+import java.util.stream.Collectors
+import kotlin.random.Random
+
+
+internal class WebViewActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityWebviewBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWebviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ok.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+            }
+        })
+        binding.ok.setOnClickListener{v: View->}
+        binding.ok.setOnClickListener{v->}
+        binding.ok.setOnClickListener{it} // it == v == v: View
+        binding.ok.setOnClickListener{} // само собой == v == v: View
+
+        binding.ok.setOnClickListener {
+            binding.url.text.let {
+
+                //binding.webView.loadUrl(it.toString()) // TODO HW можно и без этого всего
+
+                val uri = URL(it.toString())
+
+                var myConnection: HttpURLConnection? = null
+
+                myConnection = uri.openConnection() as HttpURLConnection
+                myConnection.readTimeout = 5000
+                val handler = Handler(Looper.myLooper()!!)// то же самое что Handler(Looper.getMainLooper()) потому что мы в Activity
+                Thread{
+                    val reader = BufferedReader(InputStreamReader(myConnection.inputStream))
+                    val result = getLines(reader)
+                    Log.d("@@@",result)
+
+                    handler.post( object: Runnable {
+                        override fun run() {
+                            //binding.webView.loadData(result,"text/html; charset=utf-8","utf-8")
+                            binding.webView.loadDataWithBaseURL(null,result,"text/html; charset=utf-8","utf-8",null)
+                        }
+                    })
+
+                }.start()
+
+            }
+        }
+    }
+
+    private fun getLines(reader: BufferedReader): String {
+        return reader.lines().collect(Collectors.joining("\n"))
+    }
+
+
+
+}
+
