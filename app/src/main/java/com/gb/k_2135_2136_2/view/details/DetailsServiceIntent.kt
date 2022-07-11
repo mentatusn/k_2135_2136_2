0a412e4afc3644afe1cbe51e6b55e9a37eb66daa
Andej Nesterenko <mentatus.n@gmail.com>
2022-07-11 20:22:54 +0300

    урок 7 начали |

--- app/src/main/java/com/gb/k_2135_2136_2/view/details/DetailsServiceIntent.kt
+++ app/src/main/java/com/gb/k_2135_2136_2/view/details/DetailsServiceIntent.kt
@@ -8,8 +8,11 @@ import com.gb.k_2135_2136_2.domain.City
 import com.gb.k_2135_2136_2.model.dto.WeatherDTO
 import com.gb.k_2135_2136_2.utils.*
 import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
+import java.net.MalformedURLException
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 
@@ -22,29 +25,40 @@ class DetailsServiceIntent : IntentService("") {
         intent?.let {
             it.getParcelableExtra<City>(BUNDLE_CITY_KEY)?.let {
 
-                val uri = URL("https://api.weather.yandex.ru/v2/informers?lat=${it.lat}&lon=${it.lon}")
+                try {
+                    val uri = URL("https://api.weather.yandex.ru/v2/informers?lat=${it.lat}&lon=${it.lon}")
+                    Thread {
+                        var myConnection: HttpsURLConnection? = null
+                        myConnection = uri.openConnection() as HttpsURLConnection
+                        try {
+                            myConnection.readTimeout = 5000
+                            myConnection.addRequestProperty(
+                                YANDEX_API_KEY,
+                                BuildConfig.WEATHER_API_KEY
+                            )
 
-                Thread {
-                    var myConnection: HttpsURLConnection? = null
-                    myConnection = uri.openConnection() as HttpsURLConnection
-                    try {
-                        myConnection.readTimeout = 5000
-                        myConnection.addRequestProperty(YANDEX_API_KEY, BuildConfig.WEATHER_API_KEY)
+                            val reader = BufferedReader(InputStreamReader(myConnection.inputStream))
+                            val weatherDTO =
+                                Gson().fromJson(getLines(reader), WeatherDTO::class.java)
 
-                        val reader = BufferedReader(InputStreamReader(myConnection.inputStream))
-                        val weatherDTO = Gson().fromJson(getLines(reader), WeatherDTO::class.java)
+                            LocalBroadcastManager.getInstance(this).sendBroadcast(Intent().apply {
+                                putExtra(BUNDLE_WEATHER_DTO_KEY, weatherDTO)
+                                action = WAVE
+                            })
 
-                        LocalBroadcastManager.getInstance(this).sendBroadcast(Intent().apply {
-                            putExtra(BUNDLE_WEATHER_DTO_KEY,weatherDTO)
-                            action = WAVE
-                        })
+                        } catch (e: MalformedURLException) {
 
-                    }catch (e:Exception){
-                    }finally {
-                        myConnection.disconnect()
-                    }
-                }.start()
+                        } catch (e: IOException) {
 
+                        } catch (e: JsonSyntaxException) {
+
+                        } finally {
+                            myConnection.disconnect()
+                        }
+                    }.start()
+                }catch (e: MalformedURLException){
+
+                }
             }
 
         }
