638c6a9ee8e42a9e1636d3e76ba219e365a453d9
Andej Nesterenko <mentatus.n@gmail.com>
2022-07-28 23:41:11 +0300

    Урок 12 | учебный комит - вынес повторяющийся код перехода во фрагмент navigationTo

--- app/src/main/java/com/gb/k_2135_2136_2/MainActivity.kt
+++ app/src/main/java/com/gb/k_2135_2136_2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.gb.k_2135_2136_2.databinding.ActivityMainBinding
 import com.gb.k_2135_2136_2.lesson6.ThreadsFragment
 import com.gb.k_2135_2136_2.view.contentprovider.ContentProviderFragment
@@ -18,19 +19,19 @@ import com.google.firebase.messaging.FirebaseMessaging
 internal class MainActivity : AppCompatActivity() {
 
 
-
-
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.myRoot)
-
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, CitiesListFragment.newInstance()).commit()
         }
+        getToken()
+    }
 
+    private fun getToken() {
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
             if (!task.isSuccessful) {
                 Log.w("@@@", "Fetching FCM registration token failed", task.exception)
@@ -50,50 +51,115 @@ internal class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_threads -> {
-                supportFragmentManager.apply {
-                    beginTransaction()
-                        .add(R.id.container, ThreadsFragment())
-                        .addToBackStack("")
-                        .commitAllowingStateLoss()
-                }
+                navigationTo(ThreadsFragment())
                 true
             }
             R.id.menu_history -> {
-                supportFragmentManager.apply {
-                    beginTransaction()
-                        .replace(R.id.container, WeatherHistoryListFragment())
-                        .addToBackStack("")
-                        .commitAllowingStateLoss()
-                }
+                navigationTo(WeatherHistoryListFragment())
                 true
             }
 
-            R.id.menu_content_provider-> {
-                supportFragmentManager.apply {
-                    beginTransaction()
-                        .replace(R.id.container, (ContentProviderFragment()))
-                        .addToBackStack("")
-                        .commitAllowingStateLoss()
-                }
+            R.id.menu_content_provider -> {
+                navigationTo(ContentProviderFragment())
                 true
             }
 
-            R.id.menu_google_maps-> {
-                supportFragmentManager.apply {
-                    beginTransaction()
-                        .replace(R.id.container, (MapsFragment()))
-                        .addToBackStack("")
-                        .commitAllowingStateLoss()
-                }
+            R.id.menu_google_maps -> {
+                navigationTo(MapsFragment())
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
+    private fun navigationTo(fr: Fragment) {
+        supportFragmentManager.apply {
+            beginTransaction()
+                .add(R.id.container, fr)
+                .addToBackStack("")
+                .commitAllowingStateLoss()
+
+            val s = Soldier("eregh")
+
+            s.instrument?.run {
+                excavate()
+            }
+
+            foo(Soldier("eregh"))
+
+            foo(SoldierWithShovel("eregh"))
+        }
+    }
 
 
 }
+
+abstract class Shovel{
+    abstract fun excavate()
+}
+sealed class Instruments:Shovel() {
+    data class ShovelThin(val type: Int) : Instruments(){
+        override fun excavate() {
+// особенности
+        }
+    }
+    data class ShovelWide(val type: Int) : Instruments(){
+        override fun excavate() {
+// особенности
+        }
+    }
+}
+
+fun interface Runnable {
+    fun run()
+}
+
+fun interface Flyable {
+    fun fly()
+}
+
+fun interface Excavate {
+    fun excavate()
+}
+
+fun foo(soldier: Flyable) {
+    soldier.fly()
+}
+
+open class Soldier(val name: String) : Flyable {
+    val instrument: Instruments? = null
+    fun calc() {
+        getWork()
+    }
+
+    open fun getWork() {
+        //
+    }
+
+    fun getWork2() {
+        //
+    }
+
+    fun build() {
+        getWork()
+    }
+
+    override fun fly() {
+        TODO("Not yet implemented")
+    }
+}
+
+
+
+class SoldierWithShovel(name: String) : Soldier(name) {
+    override fun getWork() {
+        // что-то сломали
+    }
+}
+
+class SoldierWithBrush(name: String) : Soldier(name) {
+}
+
 
 
 
